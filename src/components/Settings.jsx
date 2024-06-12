87d4733c1deca9d45c9377191c0b2fcfb030a13a
Saroj Belbase <siddharthbelbase@gmail.com>
2024-06-12 14:22:46 +0545

    refactor: make settings component modular

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import { SETTINGS_LANGUAGE } from '../constants';
+import { LANGUAGES, SHOW_MITI_IN_ICON } from '../constants/settings';
 
-export const Settings = ({ language, setLanguage, closeSettings }) => {
-  const handleChange = (e) => {
-    const selectedLanguage = e.target.value;
-    setLanguage(selectedLanguage);
-    localStorage.setItem('language', selectedLanguage);
+const SettingsOption = ({ id, label, value, options, onChange }) => (
+  <div className={`${id}-area`}>
+    <label htmlFor={`${id}-select`} className={`${id}-label`}>{label}</label>
+    <select id={`${id}-select`} value={value} onChange={onChange}>
+      {Object.keys(options).map((key) => (
+        <option key={key} value={options[key]}>
+          {key}
+        </option>
+      ))}
+    </select>
+  </div>
+);
+
+export const Settings = ({ language, setLanguage, showMitiInIcon, setShowMitiInIcon, closeSettings }) => {
+  const handleChange = (setter, storageKey) => (e) => {
+    const value = e.target.value;
+    setter(value);
+    localStorage.setItem(storageKey, value);
     closeSettings();
   };
 
   return (
     <div className="settings-popup visible">
-      <label htmlFor="language-select" className="language-label">Language:</label>
-      <select id="language-select" value={language} onChange={handleChange}>
-        {Object.keys(SETTINGS_LANGUAGE).map((key) => (
-          <option key={key} value={SETTINGS_LANGUAGE[key]}>
-            {key}
-          </option>
-        ))}
-      </select>
+      <SettingsOption
+        id="language"
+        label="Language:"
+        value={language}
+        options={LANGUAGES}
+        onChange={handleChange(setLanguage, 'language')}
+      />
+      <SettingsOption
+        id="badge"
+        label="Show Miti In Badge:"
+        value={showMitiInIcon}
+        options={SHOW_MITI_IN_ICON}
+        onChange={handleChange(setShowMitiInIcon, 'showMitiInIcon')}
+      />
     </div>
   );
 };
